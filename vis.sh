801478aa0dbb25736034c5dfebf2d9ca53e57f65
itsShnik <shnik456@iitkgp.ac.in>
2020-02-01 00:50:26 +0530

    added use instructions for vis.sh script

--- vis.sh
+++ vis.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+        echo "How to use (example):"
+        echo "./vis.sh ban_wa_with_fusion 12"
+        echo "12 is the maximum epoch number in the folder (starting from 0)"
+        exit 1
+fi
+
 i=0
 while [ $i -le $2 ]
 do
