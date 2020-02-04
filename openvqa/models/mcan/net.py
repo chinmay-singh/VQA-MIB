5753fa6876c035565aef2566ac804616ab12cb43
itsShnik <shnik456@iitkgp.ac.in>
2020-02-04 17:55:21 +0530

    mcan_wa model incoroporated into mcan

--- openvqa/models/mcan/net.py
+++ openvqa/models/mcan/net.py
@@ -47,7 +47,6 @@ class AttFlat(nn.Module):
         # x_mask shape: (batch, 1, 1, 14)
         # x_mask shape after 2 squeeze and unsqueeze: (batch, 14, 1)
 
-        
         att = att.masked_fill(
             x_mask.squeeze(1).squeeze(1).unsqueeze(2),
             -1e9
@@ -105,7 +104,8 @@ class Net(nn.Module):
                 num_layers=__C.LSTM_LAYERS,
                 batch_first=True,
                 bidirectional=False
-            )
+                )
+
         elif (__C.LSTM_NUM_DIRECTIONS is 2):
             self.lstm = nn.LSTM(
                 input_size=__C.WORD_EMBED_SIZE,
@@ -113,7 +113,8 @@ class Net(nn.Module):
                 num_layers=__C.LSTM_LAYERS,
                 batch_first=True,
                 bidirectional=True
-            )
+                )
+
         else:
             sys.exit("LSTM_NUM_DIRECTIONS should be either 1 or 2, current value is: %d" % __C.LSTM_NUM_DIRECTIONS)
 
@@ -125,10 +126,71 @@ class Net(nn.Module):
         self.attflat_img = AttFlat(__C)
         self.attflat_lang = AttFlat(__C)
 
-        # Classification layers
+        # Normalization layers
         self.proj_norm = LayerNorm(__C.FLAT_OUT_SIZE)
-        self.proj = nn.Linear(__C.FLAT_OUT_SIZE, answer_size)
 
+        # Decoder GRU and MLP
+        self.decoder_gru = nn.GRU(
+            input_size= __C.FLAT_OUT_SIZE,
+            hidden_size=__C.HIDDEN_SIZE,
+            num_layers=1,
+            batch_first=True
+        )
+
+        self.decoder_mlp = MLP(
+            in_size=__C.HIDDEN_SIZE,
+            mid_size= 2*__C.HIDDEN_SIZE,
+            out_size=answer_size,
+            dropout_r=0,
+            use_relu=True
+        )
+
+        # Classification layer
+        # self.proj = nn.Linear(__C.FLAT_OUT_SIZE, answer_size)
+
+        if (self.__C.WITH_ANSWER):
+
+            self.ans_embedding = nn.Embedding(
+                num_embeddings=token_size_ans,
+                embedding_dim=__C.WORD_EMBED_SIZE
+            )
+
+            # Loading the GloVe embedding weights
+            if __C.USE_GLOVE:
+                if not self.eval_flag:
+                    self.ans_embedding.weight.data.copy_(torch.from_numpy(pretrain_emb_ans))
+
+
+            if (__C.LSTM_NUM_DIRECTIONS is 1):
+                self.lstm_ans = nn.LSTM(
+                    input_size=__C.WORD_EMBED_SIZE,
+                    hidden_size=__C.HIDDEN_SIZE,
+                    num_layers=__C.LSTM_LAYERS,
+                    batch_first=True,
+                    bidirectional=False
+                    )
+
+            elif (__C.LSTM_NUM_DIRECTIONS is 2):
+                self.lstm_ans = nn.LSTM(
+                    input_size=__C.WORD_EMBED_SIZE,
+                    hidden_size=__C.HIDDEN_SIZE,
+                    num_layers=__C.LSTM_LAYERS,
+                    batch_first=True,
+                    bidirectional=True
+                )
+
+            self.attflat_ans = AttFlat(__C)
+            self.ans_norm = LayerNorm(__C.FLAT_OUT_SIZE)
+            self.fused_norm = LayerNorm(__C.FLAT_OUT_SIZE)
+
+            self.batch_size = int(__C.SUB_BATCH_SIZE/__C.N_GPU)
+            self.num = math.ceil(1000/self.batch_size) #313
+
+            # storing npy arrays
+            self.shape = (self.num * self.batch_size, int(__C.FLAT_OUT_SIZE)) #(batch, 1024)
+            self.z_proj = np.zeros(shape=self.shape) #(batch, 1024)
+            self.z_ans = np.zeros(shape=self.shape) #(batch, 1024)
+            self.z_fused = np.zeros(shape=self.shape) #(batch, 1024)
 
     def forward(self, frcn_feat, grid_feat, bbox_feat, ques_ix, ans_ix, step, epoch):
 
@@ -146,6 +208,7 @@ class Net(nn.Module):
         # output (batch, 14, NUM_DIRECTIONS * HIDDEN_SIZE)
         self.lstm.flatten_parameters()
         lang_feat, _ = self.lstm(lang_feat)
+
 
         # Returns (batch, 100, NUM_DIRECTIONS * HIDDEN_SIZE), (batch, 1, 1, 100)
         img_feat, img_feat_mask = self.adapter(frcn_feat, grid_feat, bbox_feat) 
@@ -176,8 +239,117 @@ class Net(nn.Module):
         # Here, lang_feat and img_feat have been multiplied by matrices in the last step to bring them to a common space so that they can be added
         # shape: (batch, FLAT_OUT_SIZE)
         proj_feat = lang_feat + img_feat
-        proj_feat = self.proj_norm(proj_feat) # (batch, 1024)
-        proj_feat = self.proj(proj_feat) #(batch, 3129)
 
-        return proj_feat
+        self.decoder_gru.flatten_parameters()
 
+        if (self.__C.WITH_ANSWER == False or self.eval_flag == True):
+
+            # Normalization
+            # (batch, FLAT_OUT_SIZE)
+            proj_feat = self.proj_norm(proj_feat) 
+
+            # Decoder
+            proj_feat, _ = self.decoder_gru(proj_feat.unsqueeze(1))
+            proj_feat = proj_feat.squeeze()
+            # (batch_size, answer_size)
+            proj_feat = self.decoder_mlp(proj_feat)
+     
+            if (self.eval_flag == True and self.__C.WITH_ANSWER == True):
+                #hack because test_engine expects multiple returns from net but only uses the first
+                return proj_feat, None 
+
+            return proj_feat
+
+        ####### WITH ANSWER ########
+        else:
+
+            # --------------------------- #
+            # ---- Answer embeddings ---- #
+            # --------------------------- #
+
+            ans_feat_mask = make_mask(ans_ix.unsqueeze(2))
+            ans_feat = self.ans_embedding(ans_ix)
+            self.lstm_ans.flatten_parameters()
+
+            # output (batch, 4, NUM_DIRECTIONS * HIDDEN_SIZE)
+            ans_feat, _ = self.lstm_ans(ans_feat)
+
+            # shape: (batch, FLAT_OUT_SIZE)
+            ans_feat = self.attflat_ans(
+                ans_feat,
+                ans_feat_mask
+            )
+
+            # ---------------------- #
+            # ---- Adding noise ---- #
+            # ---------------------- #
+
+            # randomly sample a number 'u' between zero and one
+            u = torch.rand(1).cuda()
+
+            proj_noise = self.__C.PROJ_STDDEV * torch.randn(proj_feat.shape).cuda()
+            ans_noise = self.__C.ANS_STDDEV * torch.randn(ans_feat.shape).cuda()
+            
+            ans_feat += ans_noise
+            proj_feat += proj_noise
+
+            # now we can fuse the vector
+            # (batch_size, FLAT_OUT_SIZE)
+            fused_feat = torch.add(torch.mul(u, proj_feat), torch.mul(1-u, ans_feat))
+
+            # ------------------- #
+            # ---- NORMALIZE ---- #
+            # ------------------- #
+
+            proj_feat = self.proj_norm(proj_feat) 
+            ans_feat = self.ans_norm(ans_feat)
+            fused_feat = self.fused_norm(fused_feat)
+
+            # --------------------------- #
+            # ---- SAVE THE FEATURES ---- #
+            # --------------------------- #
+
+            # For calculating Fusion Loss in train_engine
+            z_proj = proj_feat.clone().detach()
+            z_ans = ans_feat.clone().detach()
+            z_fused = fused_feat.clone().detach()
+
+            if (step < self.num):
+                self.z_proj[ (step * self.batch_size) : ((step+1) * self.batch_size) ] = proj_feat.clone().detach().cpu().numpy()
+                self.z_ans[ (step * self.batch_size) : ((step+1) * self.batch_size) ] = ans_feat.clone().detach().cpu().numpy()
+                self.z_fused[ (step * self.batch_size) : ((step+1) * self.batch_size) ] = fused_feat.clone().detach().cpu().numpy()
+
+
+            elif (step == self.num):
+                np.save(self.__C.SAVED_PATH + '/' + self.__C.VERSION + '/z_proj_' + str(epoch) + '.npy', self.z_proj)
+                np.save(self.__C.SAVED_PATH + '/' + self.__C.VERSION + '/z_ans_' + str(epoch) + '.npy', self.z_ans)
+                np.save(self.__C.SAVED_PATH + '/' + self.__C.VERSION + '/z_fused_' + str(epoch) + '.npy', self.z_fused)
+
+            elif (step == (self.num + 1)):
+                self.z_proj = np.zeros(shape=self.shape)
+                self.z_ans = np.zeros(shape=self.shape)
+                self.z_fused = np.zeros(shape=self.shape)
+
+            # ----------------- #
+            # ---- DECODER ---- #
+            # ----------------- #
+
+            # (batch_size, HIDDEN_SIZE)
+            proj_feat, _ = self.decoder_gru(proj_feat.unsqueeze(1))
+            proj_feat = proj_feat.squeeze()
+            # (batch_size, answer_size)
+            proj_feat = self.decoder_mlp(proj_feat)
+            
+            # (batch_size, HIDDEN_SIZE)
+            ans_feat, _ = self.decoder_gru(ans_feat.unsqueeze(1))
+            ans_feat = ans_feat.squeeze()
+            # (batch_size, answer_size)
+            ans_feat = self.decoder_mlp(ans_feat)
+            
+            # (batch_size, HIDDEN_SIZE)
+            fused_feat, _ = self.decoder_gru(fused_feat.unsqueeze(1))
+            fused_feat = fused_feat.squeeze()
+            # (batch_size, answer_size)
+            fused_feat = self.decoder_mlp(fused_feat)
+
+            return proj_feat, ans_feat, fused_feat, z_proj, z_ans, z_fused
