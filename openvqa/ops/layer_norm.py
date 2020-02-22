cb8abf88da887d2a53d3996a863ff8791cf3d182
Nikhil Shah <shnik456@iitkgp.ac.in>
2020-02-22 07:48:03 +0530

    added param to print a and b in layer norm, default false

--- openvqa/ops/layer_norm.py
+++ openvqa/ops/layer_norm.py
@@ -14,8 +14,11 @@ class LayerNorm(nn.Module):
         self.a_2 = nn.Parameter(torch.ones(size))
         self.b_2 = nn.Parameter(torch.zeros(size))
 
-    def forward(self, x):
+    def forward(self, x, to_print=False):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
+
+        if to_print:
+            print("Layer norm params, a = {}, b = {}".format(self.a_2, self.b_2))
 
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
